6ad98ab0fd10b70249e969017fca1000b93013a9
Optic_Fusion1 <37254722+OpticFusion1@users.noreply.github.com>
2021-08-30 04:25:13 -0400

    Completely removed backwards compatibility

--- Aurora/src/main/java/com/zenya/aurora/worldguard/WGManager.java
+++ Aurora/src/main/java/com/zenya/aurora/worldguard/WGManager.java
@@ -1,30 +1,42 @@
-package com.zenya.aurora.modern.worldguard;
+package com.zenya.aurora.worldguard;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.zenya.aurora.worldguard.WGManager;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
-public class WGManagerImpl extends WGManager {
+public class WGManager {
 
-  @Override
+  public static final WGManager INSTANCE = new WGManager();
+
+  public static WorldGuardPlugin getWorldGuard() {
+    Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
+    return (plugin == null || !(plugin instanceof WorldGuardPlugin)) ? null : (WorldGuardPlugin) plugin;
+  }
+
+  public ApplicableRegionSet getApplicableRegionSet(Player player) {
+    return getApplicableRegionSet(player.getLocation());
+  }
+
   public RegionManager getRegionManager(World world) {
     return WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world));
   }
 
-  @Override
   public ApplicableRegionSet getApplicableRegionSet(Location loc) {
     return getRegionManager(loc.getWorld()).getApplicableRegions(BukkitAdapter.asBlockVector(loc));
   }
 
-  @Override
   public <T extends Flag<?>> T registerFlag(T flag) throws FlagConflictException {
     WorldGuard.getInstance().getFlagRegistry().register(flag);
     return flag;
   }
+
 }
